3644071d1484ce4b7d6444b1e300afaf39cab856
Colin Flanagan <doobery@live.com>
2015-09-08 16:24:03 +0100

    new

---
+++ Salt/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Salt.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Salt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Salt.WebApiApplication" Language="C#" %>
